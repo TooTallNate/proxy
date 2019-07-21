548fc432b33d13c44657b109666319e065b283f8
Luigi Pinca <luigipinca@gmail.com>
2019-07-21 12:31:54 +0200

    resume the client socket when it is unpiped
    Flush any buffered data to ensure that the socket is closed and no
    connection is leaked on Node.js >= 10.0.0.

--- proxy.js
+++ proxy.js
@@ -349,6 +349,7 @@ function onconnect (req, socket, head) {
     res = null;
 
     socket.pipe(target);
+    target.once('unpipe', resume);
     target.pipe(socket);
   }
 
@@ -412,6 +413,17 @@ function onconnect (req, socket, head) {
     target.on('error', ontargeterror);
     target.on('end', ontargetend);
   });
+}
+
+/**
+ * Resumes a socket.
+ *
+ * @param {(net.Socket|tls.Socket)} socket The socket to resume
+ * @api private
+ */
+
+function resume (socket) {
+  socket.resume();
 }
 
 /**
